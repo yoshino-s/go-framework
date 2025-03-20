98ab1e437c9356fdc7691a8a958a5dcdacbcf0c8
yoshino-s <cy-cui@outlook.com>
2025-03-20 17:15:06 +0800

    chore: add log.FromContext...

--- log/log.go
+++ log/log.go
@@ -3,6 +3,7 @@ package log
 import (
 	"context"
 
+	"github.com/yoshino-s/go-framework/errors"
 	"go.uber.org/zap"
 )
 
@@ -12,11 +13,32 @@ func ContextWithLogger(ctx context.Context, logger *zap.Logger) context.Context 
 	return context.WithValue(ctx, loggerKey{}, logger)
 }
 
+func FromContextOptional(ctx context.Context) (*zap.Logger, bool) {
+	logger, ok := ctx.Value(loggerKey{}).(*zap.Logger)
+	return logger, ok
+}
+
 func FromContext(ctx context.Context) *zap.Logger {
 	logger, ok := ctx.Value(loggerKey{}).(*zap.Logger)
 	if !ok {
 		zap.L().Warn("logger not found in context")
 		return zap.L()
+	}
+	return logger
+}
+
+func FromContextOr(ctx context.Context, fallback *zap.Logger) *zap.Logger {
+	logger, ok := ctx.Value(loggerKey{}).(*zap.Logger)
+	if !ok {
+		return fallback
+	}
+	return logger
+}
+
+func FromContextX(ctx context.Context) *zap.Logger {
+	logger, ok := ctx.Value(loggerKey{}).(*zap.Logger)
+	if !ok {
+		errors.NewMissingComponentError("logger")
 	}
 	return logger
 }
